add7e41fcd8a89401595ae72ecf022ec9b159dae
Fai <chepngetichfaith46@gmail.com>
2021-06-13 01:32:26 +0300

    Change the file structure,create start.sh,models.py ,manage.py and init.py files

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,24 @@
 from flask import Flask
-from .config import DevConfig
-from flask_bootstrap import Bootstrap
-
-#Initializing application
-app = Flask(__name__)
-# Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile("config.py")
+from config import config_options
+from flask_sqlalchemy import SQLAlchemy
 
 
-# Initializing Flask Extensions
-bootstrap = Bootstrap(app)
-from app import views
+
+
+db = SQLAlchemy()
+
+def create_app(config_name):
+    app = Flask(__name__)
+
+    # Creating the main configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+
+    return app
---
+++ app/main/views.py
@@ -0,0 +1,43 @@
+from flask import render_template,flash,redirect
+from flask.helpers import url_for
+from . import main
+from .forms import RegistrationForm,LoginForm
+
+# Views
+@main.route('/')
+@main.route("/home")
+def home():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+    title = 'Home - This is my profile'
+    return render_template('index.html' ,title=title)
+
+@main.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+
+@main.route('/register',methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!','success')
+        return redirect(url_for('home'))
+        
+    '''
+    View root page function that returns the index page and its data
+    '''
+    title = 'Home - Register'
+    return render_template('register.html' ,title=title,form = form)
+
+
+@main.route('/login',methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    '''
+    View root page function that returns the index page and its data
+    '''
+    title = 'Home - Login'
+    return render_template('login.html' ,title=title,form = form )
