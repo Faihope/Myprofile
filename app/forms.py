a4b5c04a343c62900dd114ea400769b26574e493
Fai <chepngetichfaith46@gmail.com>
2021-06-12 16:17:31 +0300

    Add class login form

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,18 @@
 from flask_wtf import FlaskForm
 from flask_wtf.recaptcha import validators
-from wtforms import StringField,PasswordField,SubmitField
+from wtforms import StringField,PasswordField,SubmitField,BooleanField
 from wtforms.validators import DataRequired, Email, EqualTo,Length,Email,EqualTo
 
 class RegistrationForm():
-    username = StringField('username',validators=[DataRequired(),Length(min=2,max=20)])
-    email = StringField('email',validators[DataRequired(),Email()])
-    password = PasswordField('password',validators=[DataRequired()])
-    confirm_password = PasswordField('confirm_password',validators=[DataRequired(),EqualTo('password')])
+    username = StringField('Username',validators=[DataRequired(),Length(min=2,max=20)])
+    email = StringField('Email',validators[DataRequired(),Email()])
+    password = PasswordField('Password',validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm password',validators=[DataRequired(),EqualTo('password')])
     submit = SubmitField('Sign up')
+    
+class LoginForm():
+   
+    email = StringField('Email',validators[DataRequired(),Email()])
+    password = PasswordField('Password',validators=[DataRequired()])
+    remamber =BooleanField('Remember me')
+    submit = SubmitField('Login')
