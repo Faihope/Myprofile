bb9ef2b0e1084acf64c3b5e596d559f08c6a3d52
Fai <chepngetichfaith46@gmail.com>
2021-06-17 11:57:29 +0300

    heroku deployment

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-export SECRET_KEY=qwerty
-export MAIL_USERNAME=faith.chepngetich@student.moringaschool.com
-export MAIL_PASSWORD=Chepngetich46@
+export SECRET_KEY='qwerty'
+export MAIL_USERNAME='faith.chepngetich@student.moringaschool.com'
+export MAIL_PASSWORD='Chepngetich46@''
 
 python3.8 manage.py server
